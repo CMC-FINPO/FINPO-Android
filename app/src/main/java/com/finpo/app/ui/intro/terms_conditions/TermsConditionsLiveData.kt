72b63684b10228d0f6d5ff4603a4568d3cce48d0
koownij <koownij@kakao.com>
2022-06-10 17:50:32 +0900

    [REFACTOR] 약관 동의 - checkbox 양방향 바인딩 적용

--- app/src/main/java/com/finpo/app/ui/intro/terms_conditions/TermsConditionsLiveData.kt
+++ app/src/main/java/com/finpo/app/ui/intro/terms_conditions/TermsConditionsLiveData.kt
@@ -9,31 +9,14 @@ class TermsConditionsLiveData @Inject constructor() {
     private val _isCheckedAll = MutableLiveData<Boolean>()
     val isCheckedAll: LiveData<Boolean> = _isCheckedAll
 
-    private val _isCheckedTermsConditions = MutableLiveData<Boolean>()
-    val isCheckedTermsConditions: LiveData<Boolean> = _isCheckedTermsConditions
-
-    private val _isCheckedPersonalInfo = MutableLiveData<Boolean>()
-    val isCheckedPersonalInfo: LiveData<Boolean> = _isCheckedPersonalInfo
-
-    private val _isCheckedMarketing = MutableLiveData<Boolean>()
-    val isCheckedMarketing: LiveData<Boolean> = _isCheckedMarketing
+    val isCheckedTermsConditions = MutableLiveData<Boolean>()
+    val isCheckedPersonalInfo = MutableLiveData<Boolean>()
+    val isCheckedMarketing = MutableLiveData<Boolean>()
 
     fun onCheckboxAllChange(button: CompoundButton, check: Boolean) {
         _isCheckedAll.value = check
-        _isCheckedTermsConditions.value = check
-        _isCheckedPersonalInfo.value = check
-        _isCheckedMarketing.value = check
-    }
-
-    fun onCheckboxTermsConditionChange(button: CompoundButton, check: Boolean) {
-        _isCheckedTermsConditions.value = check
-    }
-
-    fun onCheckboxPersonalInfoChange(button: CompoundButton, check: Boolean) {
-        _isCheckedPersonalInfo.value = check
-    }
-
-    fun onCheckboxMarketingChange(button: CompoundButton, check: Boolean) {
-        _isCheckedMarketing.value = check
+        isCheckedTermsConditions.value = check
+        isCheckedPersonalInfo.value = check
+        isCheckedMarketing.value = check
     }
 }
