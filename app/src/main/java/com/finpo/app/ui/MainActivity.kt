feb39348ac8f55b408bdc404e9a28d8a384aa9ca
koownij <koownij@kakao.com>
2022-06-20 23:32:07 +0900

    [MODIFY] 홈 화면 레이아웃 수정 1차

--- app/src/main/java/com/finpo/app/ui/MainActivity.kt
+++ app/src/main/java/com/finpo/app/ui/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.finpo.app.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import androidx.navigation.findNavController
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.finpo.app.R
 import com.finpo.app.databinding.ActivityMainBinding
 import com.finpo.app.ui.common.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
@@ -20,6 +20,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         val navigationFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navigationFragment.navController
         NavigationUI.setupWithNavController(binding.navBar, navController)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         binding.navBar.setOnItemReselectedListener {  }
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if(destination.id == R.id.homeFragment || destination.id == R.id.communityFragment || destination.id == R.id.alarmFragment || destination.id == R.id.myPageFragment) {
@@ -29,6 +30,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             else {
                 binding.navBar.visibility = View.GONE
             }
+
+            window.statusBarColor = if(destination.id == R.id.homeFragment) ContextCompat.getColor(this, R.color.gray_g09)
+            else ContextCompat.getColor(this, R.color.white_w01)
         }
     }
 
