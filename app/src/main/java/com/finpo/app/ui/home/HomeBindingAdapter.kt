79214b7d564d9100a545cb09fc4bf08f062d82e0
koownij <koownij@kakao.com>
2022-06-22 17:31:47 +0900

    [FIX] 정책 검색 item 지역명 순서 올바르게 설정

--- app/src/main/java/com/finpo/app/ui/home/HomeBindingAdapter.kt
+++ app/src/main/java/com/finpo/app/ui/home/HomeBindingAdapter.kt
@@ -5,7 +5,11 @@ import androidx.databinding.BindingAdapter
 import com.finpo.app.R
 import com.finpo.app.model.remote.RegionInterest
 
-@BindingAdapter("region")
-fun setRegionText(textView: TextView, region: RegionInterest) {
-    textView.text = textView.context.getString(R.string.region_text, region.name, region.parent?.name ?: "전체")
+@BindingAdapter("regionData")
+fun setRegionText(textView: TextView, regionData: RegionInterest) {
+    val (detailRegion, region) =
+        if(regionData.parent == null) arrayOf("전체", regionData.name ?: "")
+     else arrayOf(regionData.name, regionData.parent.name)
+
+    textView.text = textView.context.getString(R.string.region_text, region, detailRegion)
 }
