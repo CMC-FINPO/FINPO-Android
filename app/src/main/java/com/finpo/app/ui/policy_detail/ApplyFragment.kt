5067e5efb8b15a36ea87d23bb844f9fedb3a8b6a
koownij <koownij@kakao.com>
2022-07-01 16:14:04 +0900

    [MODIFY] 정책 상세 정보 레이아웃 완성

--- app/src/main/java/com/finpo/app/ui/policy_detail/ApplyFragment.kt
+++ app/src/main/java/com/finpo/app/ui/policy_detail/ApplyFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.finpo.app.R
 import com.finpo.app.databinding.FragmentApplyBinding
 import com.finpo.app.ui.common.BaseFragment
 
 class ApplyFragment : BaseFragment<FragmentApplyBinding>(R.layout.fragment_apply) {
+    private val viewModel by viewModels<PolicyDetailViewModel>({requireParentFragment()})
     override fun doViewCreated() {
-
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
     override fun onResume() {
