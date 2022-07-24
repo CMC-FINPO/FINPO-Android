35eb0e0529cd54ae59c22ebc47fe42f89affc1ab
koownij <koownij@kakao.com>
2022-07-24 12:40:39 +0900

    [FIX] 시간 형식 수정

--- app/src/main/java/com/finpo/app/utils/TimeFormatter.kt
+++ app/src/main/java/com/finpo/app/utils/TimeFormatter.kt
@@ -10,7 +10,7 @@ object TimeFormatter {
                 if (yearMonthTime[1][0] == '0') yearMonthTime[1][1].toString() else yearMonthTime[1]
             val day = if (dayTime[0][0] == '0') dayTime[0][1].toString() else dayTime[0]
             val time = dayTime[1].split(":")
-            var hour = time[0].toInt() + 9
+            var hour = time[0].toInt()
             val min = time[1]
             var ampm = ""
             when {
