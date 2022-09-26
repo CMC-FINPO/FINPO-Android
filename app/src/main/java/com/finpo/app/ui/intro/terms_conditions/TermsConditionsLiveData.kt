40cbaa5cae8215c6924bb7600d7ee42991cd3a40
이진욱 <koownij@kakao.com>
2022-09-26 20:04:48 +0900

    [ADD] 만 14세 이상 동의 버튼 추가

--- app/src/main/java/com/finpo/app/ui/intro/terms_conditions/TermsConditionsLiveData.kt
+++ app/src/main/java/com/finpo/app/ui/intro/terms_conditions/TermsConditionsLiveData.kt
@@ -26,19 +26,25 @@ class TermsConditionsLiveData @Inject constructor() {
     private val _isCheckedMarketing = MutableLiveData<Boolean>()
     val isCheckedMarketing: LiveData<Boolean> = _isCheckedMarketing
 
+    private val _isCheckedOver14 = MutableLiveData<Boolean>()
+    val isCheckedOver14: LiveData<Boolean> = _isCheckedOver14
+
     val isTermsConditionsButtonEnabled = MediatorLiveData<Boolean>().apply {
-        addSourceList(_isCheckedTermsConditions, _isCheckedPersonalInfo) {
+        addSourceList(_isCheckedTermsConditions, _isCheckedPersonalInfo, _isCheckedOver14) {
             isTermsConditionsValid()
         }
     }
 
-    private fun isTermsConditionsValid(): Boolean = _isCheckedPersonalInfo.value == true && _isCheckedTermsConditions.value == true
+    private fun isTermsConditionsValid(): Boolean = _isCheckedPersonalInfo.value == true
+            && _isCheckedTermsConditions.value == true
+            && _isCheckedOver14.value == true
 
     init {
         _isCheckedAll.value = false
         _isCheckedTermsConditions.value = false
         _isCheckedPersonalInfo.value = false
         _isCheckedMarketing.value = false
+        _isCheckedOver14.value = false
     }
 
     fun clickCheckboxAll(check: Boolean) {
@@ -53,6 +59,7 @@ class TermsConditionsLiveData @Inject constructor() {
         _isCheckedTermsConditions.value = check
         _isCheckedPersonalInfo.value = check
         _isCheckedMarketing.value = check
+        _isCheckedOver14.value = check
     }
 
     fun onCheckboxTermsConditionsChange(button: CompoundButton, check: Boolean) {
@@ -70,9 +77,15 @@ class TermsConditionsLiveData @Inject constructor() {
         changeIsCheckedAll()
     }
 
+    fun onCheckboxOver14Change(button: CompoundButton, check: Boolean) {
+        _isCheckedOver14.value = check
+        changeIsCheckedAll()
+    }
+
     private fun changeIsCheckedAll() {
         _isCheckedAll.value = (_isCheckedTermsConditions.value!! &&
                 _isCheckedPersonalInfo.value!! &&
-                _isCheckedMarketing.value!!)
+                _isCheckedMarketing.value!! &&
+                _isCheckedOver14.value!!)
     }
 }
